75e4a922f45ec559cdc2d95bc1060bbbcce94e68
student <student@localhost>
2019-02-05 22:07:14 +0300

    Image added

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,16 @@
-
 const ClickEl = document.getElementById('button');
 const ConsumptionEl = document.getElementById('consumption');
 const FuelEl = document.getElementById('fuel');
 const ResultEl = document.getElementById('result');
 
+
 function Distance(consumption, fuel) {
     const sum = 100/consumption*fuel;
-
-
 
     ResultEl.textContent=sum;
 
 
 }
-
 ClickEl.addEventListener(
     'click',
     function(){Distance (parseInt(ConsumptionEl.value), parseInt(FuelEl.value))}
